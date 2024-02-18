781fb29f57023d40ffc60a1b9d37c595b1aabac2
Gabriel <Gabriel@GABRIEL>
2024-02-18 20:01:51 +0200

    Add project files.

---
+++ Tema1_Ex6/Program.cs
@@ -0,0 +1,16 @@
+﻿/* EX 6
+Scrieti un program care va verifica daca un numar citit de la tastatura este par sau
+impar
+*/
+
+Console.WriteLine("Introduceti un numar:");
+int numar = int.Parse(Console.ReadLine());
+
+if (numar % 2 == 0)
+{
+    Console.WriteLine("par");
+}
+else
+{
+    Console.WriteLine("impar");
+}
